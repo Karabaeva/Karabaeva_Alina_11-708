10a834f6db8ab07e3be891ff26b81d9d40b9597a
Karabaeva_Alina_11-708 <https://github.com/Karabaeva/Karabaeva_Alina_11-708>
2017-09-15 21:00:04 +0300

    My first commit

---
+++ 2017/FALL/SEM/1/Expr5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Progg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Y1 = int.Parse(Console.ReadLine());
+            int Y2 = int.Parse(Console.ReadLine());
+            int Years = (Y2 - Y1);
+            int Visocos = ((Years / 4) + (Years / 100) + (Years / 400));
+            Console.WriteLine(Visocos);
+
+           
+
+        }
+    }
+}
---
+++ 2017/FALL/SEM/1/Expr6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proggrammm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int x2 = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int y2 = int.Parse(Console.ReadLine());
+            double P = (x2 - x);
+            double Q = (y2 - y);
+            double A = Math.Sqrt((P * P) + (Q * Q));
+            Console.WriteLine(A);
+
+        }
+    }
+}

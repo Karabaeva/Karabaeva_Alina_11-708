15816b94c44fcf3cdabc0743af8c56bd25f9720f
Karabaeva_Alina_11-708 <https://github.com/Karabaeva/Karabaeva_Alina_11-708>
2017-09-18 14:45:50 +0300

    My sem ulearn

---
+++ 2017/FALL/SEM/1/Expr10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int amount = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    amount += i;
+            }
+                Console.WriteLine(amount); 
+            
+
+
+        }
+    }
+}

3d44c367d3d869e3df99487a74bbebc06d465fe2
Karabaeva_Alina_11-708 <https://github.com/Karabaeva/Karabaeva_Alina_11-708>
2017-09-20 21:14:33 +0300

    My commit

---
+++ 2017/FALL/SEM/1/Expr11.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Expr11
+{
+    class Program
+    {
+        static void Main(string[] args)
+       {
+            int hours = int.Parse(Console.ReadLine());
+            int minute = int.Parse(Console.ReadLine());
+            if ((minute * 6) < 180 || minute*6==180)
+                Console.WriteLine((minute * 6) - (hours * 30));
+            else Console.WriteLine((360 - minute * 6) + (360- hours * 30));
+
+
+
+        }
+        }
+}

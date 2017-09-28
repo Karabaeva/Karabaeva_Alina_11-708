dd8b9204e184b865c3beda28ac7af64e06f3f91a
Karabaeva_Alina_11-708 <https://github.com/Karabaeva/Karabaeva_Alina_11-708>
2017-09-28 13:20:19 +0300

    Ulearn

---
+++ 2017/FALL/SEM/1/Expr12.Ошибки Ulean.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace E12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int t = int.Parse(Console.ReadLine());
+            int v = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            double v1 = (double)h / t;
+            double tmax, tmin;
+            if (v1 <= x)
+            {
+                tmax = h / (x + 1);
+                tmin = 0;
+                Console.WriteLine(tmin);
+                Console.WriteLine(tmax);
+                
+            }
+            else
+            {
+                tmax = t;
+                tmin = (h - x * t) / (v - x);
+                Console.WriteLine(tmin);
+                Console.WriteLine(tmax);
+            }
+        }
+}   }

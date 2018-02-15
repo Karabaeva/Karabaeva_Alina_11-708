02f7ac02e441086de3b6b58e7c60d4eec9786d8e
Karabaeva_Alina_11-708 <https://github.com/Karabaeva/Karabaeva_Alina_11-708>
2018-02-15 16:59:45 +0300

    7 задание

---
+++ Inform/AlinaKarabaeva_7task.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(array[i]);
+            }
+            for (int j=0;j<array.Length-1;j++)
+            {
+                if (array[j] < array[j+1])
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+
+    }
+}

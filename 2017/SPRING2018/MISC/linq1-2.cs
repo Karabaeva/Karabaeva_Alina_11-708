948de99dbc41079db89efda9493568ad7894cb9d
Karabaeva_Alina_11-708 <https://github.com/Karabaeva/Karabaeva_Alina_11-708>
2018-03-29 13:12:30 +0300

    LINQ 1.2

---
+++ 2017/SPRING2018/MISC/linq1-2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq2
+{
+    class Program
+    {
+
+        static int k = int.Parse(Console.ReadLine());
+        public static int K { get => k; set => k = value; }
+
+        static void Main(string[] args)
+        {
+           
+            List<int> numbers = new List<int>() { 5,8,7,2,10,4,8,5,2,4,7,6 };
+            SeparationOfElements(numbers);
+            Console.ReadLine();
+        }
+        static void SeparationOfElements(IEnumerable<int> num)
+        {
+            var evemelem = num.Where(t => t % 2 == 0).Select(t => t);
+            int[] evenArray = evemelem.ToArray();
+
+            var morethan = num.Skip(K);
+            int[] thanArray = morethan.ToArray();
+            DifferenceOfSets(evenArray, thanArray);
+        }
+        static void DifferenceOfSets(int[] first, int[] second)
+        {
+            var difference = second.Except(first).Reverse();
+            foreach (var end in difference)
+                Console.WriteLine(end);
+            
+        }
+       
+    }
+}
